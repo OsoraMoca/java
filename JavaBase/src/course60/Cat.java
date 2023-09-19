638e48959ea59902e62da1b0347cee7e35de209e
S18 <S18@MSI>
2023-09-19 10:33:03 +0900

    ポリモーフィズムとオーバーライドのプログラム #3

---
+++ JavaBase/src/course60/Cat.java
@@ -0,0 +1,10 @@
+package course60;
+
+public class Cat extends Animal {
+	
+	@Override
+	public void nakigoe() {
+		System.out.println("ニャーニャー");
+	}
+
+}
---
+++ JavaBase/src/course60/Dog.java
@@ -0,0 +1,10 @@
+package course60;
+
+public class Dog extends Animal {
+	
+	@Override
+	public void nakigoe() {
+		System.out.println("ワンワン");
+	}
+	
+}
